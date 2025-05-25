57594a0237a3add1e7d1132bcdc6184efa3b1be7
Tom Felici <tfelici@gmail.com>
2025-05-25 16:34:22 +0100

    -

--- app.py
+++ app.py
@@ -451,4 +451,4 @@ def restart_services():
     return jsonify({'success': True, 'results': results})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
